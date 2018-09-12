5b16200f47d716159a33a2a1049010f68092e24c
vidushisomani <vidsa99@gmail.com>
2018-09-12 18:18:46 -0500

    Commit Solved Maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,12 +35,25 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnRight();
+                if (!maze.canMove()) {
+                    maze.turnLeft();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnLeft();
+                        maze.move();
+                    }
+                } else {
+                    maze.move();
+                }
+            }
         }
-
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
-        }
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
     }
 }
